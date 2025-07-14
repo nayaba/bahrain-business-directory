a6356420093e0cd5f6d813e4e7b4aa6ea7d88c7b
Nabila Ayaba <nabilaayaba@gmail.com>
2025-07-14 11:31:02 +0300

    Create business new and post route

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose')
 const app = express()
 const path = require('path')
 
+// CONTROLLERS
+const businessController = require('./controllers/businessController')
+
 // DATABASE CONNECTION
 mongoose.connect(process.env.MONGODB_URI)
 mongoose.connection.on('connected', () => {
@@ -13,11 +16,15 @@ mongoose.connection.on('connected', () => {
 
 // MIDDLEWARE
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: false }))
 
 // GET / (home)
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
+
+// ROUTES
+app.use('/businesses', businessController)
 
 app.listen(3000, () => {
     console.log('Listening on port 3000')
