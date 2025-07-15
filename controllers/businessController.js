510116f0e1dced6545f8f386c01c5eabae8da0c2
Nabila Ayaba <nabilaayaba@gmail.com>
2025-07-15 11:32:08 +0300

    Finish CRUD

--- controllers/businessController.js
+++ controllers/businessController.js
@@ -42,6 +42,17 @@ router.get('/:businessId/edit', async (req, res) => {
     const foundBusiness = await Business.findById(req.params.businessId)
     res.render('businesses/edit.ejs', {foundBusiness: foundBusiness})
 })
-// controller function should render 'businesses/edit.ejs' <--- ejs file should have edit form
+
+
+// PUT for submitting the form
+router.put('/:businessId', async (req, res) => {
+    if (req.body.isVerified === 'on') {
+        req.body.isVerified = true
+    } else {
+        req.body.isVerified = false
+    }
+    await Business.findByIdAndUpdate(req.params.businessId, req.body)
+    res.redirect(`/businesses/${req.params.businessId}`)
+})
 
 module.exports = router
